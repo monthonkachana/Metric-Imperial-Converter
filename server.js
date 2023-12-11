cbc30b0146939d52eda1d6ec4cf126d56a0eb74b
monthonkachana <maxmonthonkachana2@gmail.com>
2023-12-11 10:54:27 +0700

    V2

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const expect = require('chai').expect;
 const cors = require('cors');
 require('dotenv').config();
+// require('dotenv').config();
+// require('dotenv').config();
 
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
